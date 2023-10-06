0dfee8b3c8f16877af1b33ee637d1ab6d7339dab
Xianyou101 <adimasputra101@gmail.com>
2023-10-06 15:11:40 +0700

    04 Perulangan

---
+++ 04 Perulangan/loop01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  
+  for (int c = 1; c <= 10; c++)
+  {
+  cout << c << " aku sayang tita " << endl;
+
+  }
+
+}
---
+++ 04 Perulangan/loop03.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    while (true){
+        cout << "lupakan masa lalu..." << endl;
+    }
+    return 0;
+}
